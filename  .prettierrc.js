18d2f910d2d7832b592c7eabdf62e6a6d9217b17
xiajing7478 <260594736@qq.com>
2023-12-31 14:08:59 +0800

    add chalk
    add chalk

---
+++  .prettierrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    // 一行最多 120 字符
+    printWidth: 120,
+    // 使用 2 个空格缩进
+    tabWidth: 2,
+    // 不使用 tab 缩进，而使用空格
+    useTabs: false,
+    // 行尾需要有分号
+    semi: true,
+    // 使用单引号代替双引号
+    singleQuote: true,
+    // 对象的 key 仅在必要时用引号
+    quoteProps: 'as-needed',
+    // jsx 不使用单引号，而使用双引号
+    jsxSingleQuote: false,
+    // 末尾使用逗号
+    trailingComma: 'all',
+    // 大括号内的首尾需要空格 { foo: bar }
+    bracketSpacing: true,
+    // jsx 标签的反尖括号需要换行
+    jsxBracketSameLine: false,
+    // 箭头函数，只有一个参数的时候，也需要括号
+    arrowParens: 'always',
+    // 每个文件格式化的范围是文件的全部内容
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    // 不需要写文件开头的 @prettier
+    requirePragma: false,
+    // 不需要自动在文件开头插入 @prettier
+    insertPragma: false,
+    // 使用默认的折行标准
+    proseWrap: 'preserve',
+    // 根据显示样式决定 html 要不要折行
+    htmlWhitespaceSensitivity: 'css',
+    // 换行符使用 lf
+    endOfLine: 'lf'
+}
+
+作者：李永宁
+链接：https://juejin.cn/post/6901552013717438472
+来源：稀土掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# kitt-cli111111
+# mkdir ts-project && cd ts-project && npm init -y && npm i typescript -D && npx tsc --init
+
+这条命令的意思是在当前目录下创建一个 ts-project 目录，然后进入 ts-project 目录执行 npm init -y 初始话目录产生 package.json 文件，之后运行 npm i typescript -D 在开发环境安装 typescript 包，之后执行 npx tsc --init 生成 tsconfig.json 文件
